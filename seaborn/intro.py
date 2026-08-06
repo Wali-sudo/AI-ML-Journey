e648c44d5d95db165a1cbbda59df1a2f069343c7
Wali-Sudo <waliahmad25906@gmail.com>
2026-08-06 07:44:22 +0500

    Done with intro.py in seaborn

---
+++ seaborn/intro.py
@@ -0,0 +1,10 @@
+#kaggle
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pandas as pd
+spotify_data=pd.read_csv('/home/waliahmad/Downloads/most_streamed_spotify_2025.csv')
+print(spotify_data.head())
+
+plt.figure(figsize=(12,6))
+sns.lineplot(data=spotify_data)
+plt.show()
