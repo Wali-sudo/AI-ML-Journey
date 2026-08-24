52483f650acb856fd6d3545d0cb4a75f037dd278
Wali-Sudo <waliahmad25906@gmail.com>
2026-08-24 23:51:15 +0500

    Updated Random_forest.py in Week-2

--- Week2/Intro/random_forest.py
+++ Week2/Intro/random_forest.py
@@ -13,7 +13,9 @@ x = filtered_melb_data[melbourne_features]
 
 train_x,val_x,train_y,val_y=train_test_split(x,y,random_state=0)
 
-model=RandomForestRegressor(n_estimators=1000,random_state=0)
+model=RandomForestRegressor(n_estimators=1000,random_state=0)#another parameter criterion is used which determines how each decision tree inside the forest measures split quality when building its branches.
+#squared_error: Squares the errors, which heavily penalizes large mistakes. Outliers (e.g., a single $10,000,000 mansion) will pull the tree's split points toward them.
+#absolute_error: Treats all errors linearly. Outliers have a much smaller effect on the tree's splits, making the model more robust to noisy data.
 model.fit(train_x,train_y)
 pred_val=model.predict(val_x)
 my_mae=mean_absolute_error(val_y,pred_val)
